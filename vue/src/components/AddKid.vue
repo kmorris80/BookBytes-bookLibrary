f30de6f8dc7fab3d41bb39f076f115c72e43b7c2
Karen Morris <bykarenmorris@gmail.com>
2022-05-31 15:28:15 -0500

    pushing changes to kidCard

---
+++ vue/src/components/AddKid.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="text-center">
+   <form>
+        <div>
+      <h1 class="h3 mb-3 font-weight-normal">Create Kid Account</h1>
+      </div>
+      <label for="name" class="sr-only">Name:</label>
+      <input type="text" id="username" class="form-control">
+      <label for="name" class="sr-only">Age:</label>
+      <input type="text" id="username" class="form-control">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">
+        Create Account
+      </button> 
+    </form> 
+  </div> 
+</template>
+
+
+
+<style scoped>
+
+input {
+  margin-bottom: 20px;
+  margin-top: 5px;
+  border: solid 1px;
+}
+
+</style>
---
+++ vue/src/views/AddKidView.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <add-kid></add-kid>
+</div>
+
+</template>
+
+
+<script>
+import addKid from "../components/AddKid.vue"
+
+export default {
+    name: "addKid",
+    components:{
+        addKid
+    }
+}
+
+
+
+</script>
