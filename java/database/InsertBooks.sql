618b304e6bc095a4dae7676137f7efc0d6d84684
Shaun Hughes <shaunhugs23@gmail.com>
2022-06-02 16:17:05 -0500

    Book table population

---
+++ java/database/InsertBooks.sql
@@ -0,0 +1,94 @@
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('It Ends With Us', 'Colleen Hoover', '1501110365', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Where the Crawdads Sing', 'Delia Owens', '0735219109', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Verity', 'Colleen Hoover', '1538724731', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Power of One More: The Ultimate Guide to Happiness and Success', 'Ed Mylett', '1119815363', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Oh, the Places Youll Go! ', 'Dr.Seuss', '9780679805274', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Book Lovers', 'Emily Henry', '0593334833', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Sparring Partners', 'John Grisham', '0385549326', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Bodies Of Others: The New Authoritarians, Covid-19 and the War Against the Human', 'Naomi Wolf', '1737478560', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Happy-Go-Lucky', 'David Sedaris', '0316392456', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Meant To Be', 'Emily Griffin', '0425286649', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Every Summer After', 'Carley Fortune', '0593438531', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Atomic Habits: An Easy & Proven Way to Build Good & Break Bad Ones', 'James Clear', '0735211299', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Ugly Love', 'Colleen Hoover', '1476753180', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Seven Husbands of Evelyn Hugo', 'Taylor Jenkins Reid', '1501161938', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Plot Against the King', 'Kash Patel', '1955550123', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Reminders of Him', 'Colleen Hoover', '1542025605', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Very Hungry Catepillar', 'Eric Carle ', '0399226907', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The 48 Laws of Power', 'Robert Greene', '0140280197', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Kindergarten, Here I Come!', 'D.J. Steinberg', '0448456249', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('November 9', 'Colleen Hoover', '1501110349', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('One Italian Summer', 'Rebecca Serle', '1982166797', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Every Summer After', 'Carley Fortune', '0593438531', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Beach Read', 'Emily Henry', '1984806734', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('People We Meet on Vacation', 'Emily Henry', '1984806750', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Finding Me: A Memoir', 'Viola Davis', '0063037327', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('If Animals Kissed Good Night', 'Ann Whitford Paul', '0374300216', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Hotel Nantucket', 'Elin Hilderbrand', '0316258679', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Things We Never Got Over', 'Lucy Score', '194563183X', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Last Thing He Told Me', 'Laura Dave', '1501171348', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Endure: How to Work Hard, Outlast, and Keep Hammering', 'Cameron Hanes', '1250279291', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Silent Patient', 'Alex Michaelides', '125030170X', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('The Midnight Library', 'Matt Haig', '0525559477', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('It Starts With Us', 'Colleen Hoover', '1668001225', 'true');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('1984', 'George Orwell', '9780451524935', 'false');
+INSERT INTO book (title, author, isbn, new_release)
+VALUES ('Becoming Supernatural: How Common People Are Doing the Uncommon', 'Dr. Joe Dispenza', '1781808317', 'false');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
