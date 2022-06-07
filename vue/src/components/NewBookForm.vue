1a6c6874c28de460c8f60d80528167b244d67a06
Austin O'Donnell <odonnell.austino@gmail.com>
2022-06-06 19:36:06 -0700

    status code changes to new book form to get message

--- vue/src/components/NewBookForm.vue
+++ vue/src/components/NewBookForm.vue
@@ -1,7 +1,7 @@
 // <template> 
 <!-- //wrap in a v-if $store.state.user.permission - allow show page, if not show "you don't have access" -->
   <div class="text-center">
-    <form class="new-book-form" v-on:submit.prevent="saveBook">
+    <form class="new-book-form" v-on:submit="saveBook">
       <header>
         <img id = "NewBookForm" src="../assets/NewBookForm.png"/>
         </header>
@@ -14,36 +14,42 @@
           type="text"
           placeholder="Title"
           v-model="book.title"
+          required
         />
         <input
           class="author-input form-control"
           type="text"
           placeholder="Author"
           v-model="book.author"
+          required
         />
         <input
           class="form-control"
           type="text"
           placeholder="Genre"
           v-model="book.genre"
+          required
         />
         <input
           class="form-control"
           type="text"
           placeholder="Keyword"
           v-model="book.keyword"
+          required
         />
         <input
           class="form-control"
           type="text"
           placeholder="Character"
           v-model="book.character"
+          required
         />
          <input
           class="form-control"
           type="text"
           placeholder="ISBN"
           v-model="book.isbn"
+          required
         />
       </div>
       <!-- <div>
@@ -80,34 +86,16 @@ export default {
     };
   },
   methods: {
-    
-      // const newBook ={
-      //   title: this.book.title,
-      //   author: this.book.author,
-      //   genre: this.book.genre,
-      //   keyword: this.book.keyword,
-      //   character: this.book.character
-      // }
        saveBook() {
         bookService
         .create(this.book)
         .then((response)=>{
-          if(response.status === 201){
-            this.$router.push(`/addBook`)
-            window.alert("Book Created!");
+          console.log(response.status)
+          if(response.status === 200){
+            window.alert("Book Created!"); 
           }
-        })
-        // .catch(error =>{
-
-       
+        });
       }
-      //alternative method for adding book?
-      // bookService.create(this.book).then((response) => {
-      //   if (response.status == 201) {
-      //    
-      //     this.$router.push("/addBook");
-      //   }
-      // }
     }
   }
 
