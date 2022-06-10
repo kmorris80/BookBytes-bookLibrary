e145760133ad8196300b4ecdbe4e9b4ca530a2a0
Shaun Hughes <shaunhugs23@gmail.com>
2022-06-09 20:10:50 -0500

    insert book

--- java/database/InsertBooks.sql
+++ java/database/InsertBooks.sql
@@ -5,71 +5,16 @@ VALUES ('Where the Crawdads Sing', 'Delia Owens', '0735219109', 'Kya' , 'Fiction
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
 VALUES ('Verity', 'Colleen Hoover', '1538724731', 'Lowen', 'Thriller','Crawford', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('The Power of One More: The Ultimate Guide to Happiness and Success', 'Ed Mylett', '1119815363', 'N/A', 'Self Help', 'Ultimate', 'true');
-
-
+VALUES ('The Da Vinci Code', 'Dan Brown', '0307474275', 'Robert Langdon', 'Crime', 'cyrptology', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Oh, the Places Youll Go! ', 'Dr.Seuss', '9780679805274', 'false');
+VALUES ('Twilight', 'Stephanie Meyer', '0316015849', 'Edward Cullen', 'Fiction', 'vampires', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Book Lovers', 'Emily Henry', '0593334833', 'true');
+VALUES ('The Lovely Bones', 'Alice Shebold', '0316168815', 'Susie Salmon', 'Fiction', 'killer', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Sparring Partners', 'John Grisham', '0385549326', 'true');
+VALUES ('Life of Pi', 'Yann Martel ', '9780156027328', 'Pi Patel', 'Fiction', 'tiger', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('The Bodies Of Others: The New Authoritarians, Covid-19 and the War Against the Human', 'Naomi Wolf', '1737478560', 'true');
+VALUES ('The Broker', 'John Grisham', '0345532007', 'Joel Backman', 'Crime', 'CIA', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Happy-Go-Lucky', 'David Sedaris', '0316392456', 'true');
+VALUES ('Hannibal', 'Thomas Harris', '0440224675', 'Hannibal Lecter', 'Crime', 'cannibal', 'false');
 INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Meant To Be', 'Emily Griffin', '0425286649', 'true');
-INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Every Summer After', 'Carley Fortune', '0593438531', 'true');
-INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Atomic Habits: An Easy & Proven Way to Build Good & Break Bad Ones', 'James Clear', '0735211299', 'false');
-INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('Ugly Love', 'Colleen Hoover', '1476753180', 'false');
-INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-VALUES ('The Seven Husbands of Evelyn Hugo', 'Taylor Jenkins Reid', '1501161938', 'false');
-INSERT INTO book (title, author, isbn, character, genre, keyword, new_release)
-
-
-
-VALUES ('The Plot Against the King', 'Kash Patel', '1955550123', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Reminders of Him', 'Colleen Hoover', '1542025605', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The Very Hungry Catepillar', 'Eric Carle ', '0399226907', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The 48 Laws of Power', 'Robert Greene', '0140280197', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Kindergarten, Here I Come!', 'D.J. Steinberg', '0448456249', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('November 9', 'Colleen Hoover', '1501110349', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('One Italian Summer', 'Rebecca Serle', '1982166797', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Every Summer After', 'Carley Fortune', '0593438531', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Beach Read', 'Emily Henry', '1984806734', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('People We Meet on Vacation', 'Emily Henry', '1984806750', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Finding Me: A Memoir', 'Viola Davis', '0063037327', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('If Animals Kissed Good Night', 'Ann Whitford Paul', '0374300216', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The Hotel Nantucket', 'Elin Hilderbrand', '0316258679', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Things We Never Got Over', 'Lucy Score', '194563183X', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The Last Thing He Told Me', 'Laura Dave', '1501171348', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Endure: How to Work Hard, Outlast, and Keep Hammering', 'Cameron Hanes', '1250279291', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The Silent Patient', 'Alex Michaelides', '125030170X', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('The Midnight Library', 'Matt Haig', '0525559477', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('It Starts With Us', 'Colleen Hoover', '1668001225', 'true');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('1984', 'George Orwell', '9780451524935', 'false');
-INSERT INTO book (title, author, isbn, new_release)
-VALUES ('Becoming Supernatural: How Common People Are Doing the Uncommon', 'Dr. Joe Dispenza', '1781808317', 'false');
+VALUES ('The Lord of the Rings', 'J.R.R. Tolkien', '0358439191', 'Frodo', 'Fantasy', 'Middle earth', 'false');
